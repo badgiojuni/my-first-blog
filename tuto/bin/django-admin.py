b92c1a195e9a593e225869e70b9bb4a515942e3d
Theophile Baggio <theobaggio@hotmail.fr>
2021-12-14 13:07:22 +0100

    coucou

---
+++ tuto/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/theophilebaggio/Desktop/studies/Lyon_M2_SITN/Dev_info/djangogirls/tuto/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
